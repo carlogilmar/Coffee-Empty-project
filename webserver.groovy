e7af3b6f1ab81d13b81e82c5d7854007c51fe3bb
carlogilmar <carlogilmar12@gmail.com>
2017-01-15 23:34:25 -0600

    Agregando webserver

---
+++ webserver.groovy
@@ -0,0 +1,15 @@
+
+import io.vertx.groovy.ext.web.handler.StaticHandler
+import io.vertx.groovy.ext.web.Router
+import io.vertx.groovy.ext.web.handler.BodyHandler
+
+//vertx routes
+def server = vertx.createHttpServer()
+def router = Router.router(vertx)
+
+router.route().handler(BodyHandler.create())
+
+//route by defect->index
+router.route("/static/*").handler(
+	 StaticHandler.create().setCachingEnabled(false)
+)
