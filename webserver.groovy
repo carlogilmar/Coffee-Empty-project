f9d401c236916571e041e261a083c9e7a16cc590
carlogilmar <carlogilmar12@gmail.com>
2017-01-17 09:42:42 -0600

    Agregando demo de vertx

--- webserver.groovy
+++ webserver.groovy
@@ -6,12 +6,28 @@ import io.vertx.groovy.ext.web.handler.BodyHandler
 //vertx routes
 def server = vertx.createHttpServer()
 def router = Router.router(vertx)
-
+//Event Bus
+def eb = vertx.eventBus()
+//Router
 router.route().handler(BodyHandler.create())
 
 //route by defect->index
-router.route("/static/*").handler(
+router.route("/*").handler(
 	 StaticHandler.create().setCachingEnabled(false)
 )
+
+
+//Verticle-Consumer/Handler
+eb.consumer("com.makingdevs", { message ->
+  message.reply("tu me dijiste ---${message.body()}---- y yo te respondo ---holi---")
+ })
+
+//Verticle Sender
+vertx.eventBus().send("com.makingdevs", "Hola", { reply ->
+  if (reply.succeeded()) {
+    println reply.result().body()
+  }
+})
+
 
 server.requestHandler(router.&accept).listen(8080)
